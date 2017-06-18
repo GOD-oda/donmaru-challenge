a6b13fb0073c4573296898914ed56df8d6a37250
Takahiro_Oda <GOD-oda@users.noreply.github.com>
2017-06-18 16:34:37 +0900

    Add twitter oauth (#2)
    * Add twitter oauth
    
    * Update .env.example

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'login'], function () {
+    Route::get('{provider_name}', 'Auth\LoginController@redirectToProvider');
+    Route::get('{provider_name}/callback', 'Auth\LoginController@handleProviderCallback');
+});
+Route::get('logout', 'Auth\LoginController@logout');
+
+Route::get('home', function () {
+    return 'loged in!!!';
 });
