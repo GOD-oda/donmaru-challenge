30fa106f4e5e13f014866091a71ccfa698123e5b
t.oda <takahiro.tech.oda@gmail.com>
2017-08-26 18:02:28 +0900

    Rename migrattion files

--- database/migrations/2017_08_26_175525_create_posts_table.php
+++ database/migrations/2017_08_26_175525_create_posts_table.php
@@ -17,11 +17,13 @@ class CreatePostsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->integer('don_id')->unsigned();
+            $table->integer('shop_id')->unsigned();
             $table->string('single_word')->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('don_id')->references('id')->on('dons')->onDelete('cascade');
+            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
         });
     }
 
