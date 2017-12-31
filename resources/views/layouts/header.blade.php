86fa6c7399dafbf3e8a295a92a61a8474e46b0df
t.oda <takahiro.tech.oda@gmail.com>
2017-12-31 14:49:44 +0900

    Add header scss

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,26 +1,26 @@
 <header>
     <div class="header-content container">
-        <div class="header-brand">
+        <div class="page-header">
             <h1>どんまるチャレンジ</h1>
         </div>
-        <div class="header-right">
-            <div class="user-name">
-                @if ($user)
-                    {{ $user->name }}
-                @else
-                    ログインしましょう
-                @endif
-            </div>
-            @if ($user)
-                <div class="logout">
-                    <a href="/logout">ログアウト</a>
-                </div>
-            @else
-                <div class="login">
-                    <a href="/login/twitter"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
-                </div>
-            @endif
 
-        </div>
+        {{--<div class="header-right">--}}
+            {{--<div class="user-name">--}}
+                {{--@if ($user)--}}
+                    {{--{{ $user->name }}--}}
+                {{--@else--}}
+                    {{--ログインしましょう--}}
+                {{--@endif--}}
+            {{--</div>--}}
+            {{--@if ($user)--}}
+                {{--<div class="logout">--}}
+                    {{--<a href="/logout">ログアウト</a>--}}
+                {{--</div>--}}
+            {{--@else--}}
+                {{--<div class="login">--}}
+                    {{--<a href="/login/twitter"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>--}}
+                {{--</div>--}}
+            {{--@endif--}}
+        {{--</div>--}}
     </div>
 </header>
